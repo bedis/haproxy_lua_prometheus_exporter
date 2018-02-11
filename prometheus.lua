46aac1d95de4a2d207aecf63e76aca160f522b58
Baptiste Assmann <bedis9@gmail.com>
2018-02-11 15:03:51 +0100

    MINOR: FIX: missing default page content
    By default (for now) the code only honors the /metrics URL and return a
    default page for any other url.
    That said, the content to be returned was not part of this Lua script.
    
    This patch fix this issue

--- prometheus.lua
+++ prometheus.lua
@@ -10,6 +10,18 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 --]]
 
+-- default page served when the request arrive in the Lua exporter
+-- for any URL but /metrics
+local default_page = [[
+<html>
+<head><title>Haproxy Exporter in Lua</title></head>
+<body>
+<h1>Haproxy Exporter in Lua</h1>
+<p><a href='/metrics'>Metrics</a></p>
+</body>
+</html>
+]]
+
 -- table to link HAProxy's frontend statistics counter to its prometheus equivalent
 local frontendMetrics = {
 	scur = { type="gauge", metricName="haproxy_frontend_current_sessions", help="Current number of active sessions." };
